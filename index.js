6b46f403a5c7c8fb07329a336e0919accbf49437
ThinkeringAbout <104573860+ThinkeringAbout@users.noreply.github.com>
2022-10-05 16:29:16 +0300

    initial

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+// const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const admin = require('firebase-admin')
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+
+const jsonParser = bodyParser.json()
+const serviceAccount = require('./key.json');
+
+app.use(express.json())
+app.use(cors())
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = getFirestore();
+const docRef = db.collection('orders');
+const mealsRef = db.collection('meals');
+
+app.get('/meals', async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  let meals = []
+    await db.collection('meals').get().then(snapshot => {
+        snapshot.forEach((doc) => {
+          meals.push(doc.data())
+        });
+        res.send(meals)
+    });
+})
+
+app.get('/orders', async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  let orders = []
+    await db.collection('orders').get().then(snapshot => {
+        snapshot.forEach((doc) => {
+          orders.push(doc.data())
+        });
+        res.send(orders)
+    });
+})
+
+app.post('/order', jsonParser, (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  docRef.doc(req.body.id).set(req.body)
+  res.send("OK")
+})
+
+app.post('/addmeal', jsonParser, (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  mealsRef.add(req.body)
+  res.send("OK")
+})
+
+app.post('/updateorder', jsonParser, (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  // console.log(req.body.completed)
+  const statusToSend = {
+    completed: req.body.completed
+  }
+  docRef.doc(req.body.id).update(statusToSend)
+  res.send("OK")
+})
+
+// app.post('/create', async (req, res) => {
+//   const userResponse = await admin.auth().createUser({
+//     email: req.body.email,
+//     password: req.body.password,
+//   }).then((userRecord) => {
+//     console.log("Created user", userRecord.uid)
+//     res.send("Ok")
+//   }).catch((err) => {
+//     console.log("Error", err)
+//     res.send("Bad")
+//   })
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
